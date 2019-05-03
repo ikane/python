0f5801645cde3442e9fed167b0c557392d155177
ikane <kaneelhadji2610@yahoo.fr>
2019-05-03 08:44:35 +0200

    using finally

--- mosh/exception_handling.py
+++ mosh/exception_handling.py
@@ -1,5 +1,9 @@
 try:
+    file = open("file.txt")
     age = int(input("Age: "))
-    xfactor = age / 0
+    #xfactor = age / 0
 except (ValueError, ZeroDivisionError) as ex:
     print("You didn't enter a valid age.")
+    print(ex)
+finally:
+    file.close()
