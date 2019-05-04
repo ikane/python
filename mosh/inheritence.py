3c57758bdb6c9ab2b3c0ae991ee5061c9b5448df
ikane <kaneelhadji2610@yahoo.fr>
2019-05-04 14:30:54 +0200

    introduce inheritence

---
+++ mosh/inheritence.py
@@ -0,0 +1,32 @@
+class InvalidOperation(Exception):
+    pass
+
+
+class Stream:
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperation("Stream is already opened")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperation("Stream is already closed")
+        self.opened = False
+
+
+class FileStream(Stream):
+    def read(self):
+        print("Read file from file")
+
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Read file from Network")
+
+
+f = FileStream()
+f.read()
+print(isinstance(f, FileStream))
