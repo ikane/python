57f649a449ea1cafaa59aa2b136c0668203c27d0
ikane <kaneelhadji2610@yahoo.fr>
2019-05-13 04:55:08 +0200

    module and packages

--- mosh/app.py
+++ mosh/app.py
@@ -7,17 +7,24 @@ import sys
 
 # calc_shipping()
 
-sales.calc_tax()
+# sales.calc_tax()
 
 
-# print(sys.path)
+print(sys.path)
 # print(dir(sales))
 # print(sales.__name__)
 # print(sales.__package__)
 # print(sales.__file__)
 
-path = Path("ecommerce/__init__.py")
-print(path.exists())
+# path = Path("ecommerce/__init__.py")
+# print(path.exists())
 
-path = path.with_suffix(".txt")
-print(path)
+# path = path.with_suffix(".txt")
+# print(path)
+
+path = Path(r"D:\DEV\sources\python\mosh\ecommerce")
+print(path.exists())
+print(path.iterdir)
+
+for p in path.iterdir():
+    print(p)
--- mosh/ecommerce/shopping/sales.py
+++ mosh/ecommerce/shopping/sales.py
@@ -1,4 +1,4 @@
-#from ecommerce.customer import contact
+from ecommerce.customer import contact
 
 # from ..customer import contact
 
